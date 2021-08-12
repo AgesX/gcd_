a35646fb9ee1092f5ba962f11335bdf99d8dd32b
邓江洲 <dengjiangzhouReal@gmail.com>
2021-08-12 14:44:35 +0800

    //    浅拷贝， 回答错误， 一条， B 站， 比心
        //    因为其，不可变更

---
+++ 银行 face/failedAgain/failedAgain/ViewController.m
@@ -0,0 +1,45 @@
+//
+//  ViewController.m
+//  failedAgain
+//
+//  Created by Jz D on 2021/8/12.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+
+
+- (void) testOne{
+    
+    NSArray * one = @[@"123"];
+    
+    NSArray * two = [one copy];
+    
+    // NSLog(@"one  %p, \n two  %p", &one, &two);
+    NSLog(@"\n  one  %p, \n  two  %p", one, two);
+    
+    //    浅拷贝， 回答错误， 一条， B 站， 比心
+    
+    //    因为其，不可变更
+    
+}
+
+
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+    
+    
+    [self testOne];
+    
+}
+
+
+@end
