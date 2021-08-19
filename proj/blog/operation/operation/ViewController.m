92ce5141629a66bcb74d8109f418ba6a80c44e20
邓江洲 <dengjiangzhouReal@gmail.com>
2021-08-19 16:29:25 +0800

    NSLock * simpleLock;

--- proj/blog/operation/operation/ViewController.m
+++ proj/blog/operation/operation/ViewController.m
@@ -9,6 +9,10 @@
 
 @interface ViewController ()
 
+@property (nonatomic, assign) NSInteger ticketSurplusCount;
+
+@property (nonatomic, strong) NSLock * simpleLock;
+
 @end
 
 @implementation ViewController
@@ -54,6 +58,150 @@
 }
 
 
+//
+
+//
+
+
+//
+
+
+/**
+ * 非线程安全：不使用 NSLock
+ * 初始化火车票数量、卖票窗口(非线程安全)、并开始卖票
+ */
+- (void)initTicketStatusNotSave {
+    NSLog(@"currentThread---%@",[NSThread currentThread]); // 打印当前线程
+
+    self.ticketSurplusCount = 50;
+
+    // 1.创建 queue1,queue1 代表北京火车票售卖窗口
+    NSOperationQueue *queue1 = [[NSOperationQueue alloc] init];
+    queue1.maxConcurrentOperationCount = 1;
+
+    // 2.创建 queue2,queue2 代表上海火车票售卖窗口
+    NSOperationQueue *queue2 = [[NSOperationQueue alloc] init];
+    queue2.maxConcurrentOperationCount = 1;
+
+    // 3.创建卖票操作 op1
+    __weak typeof(self) weakSelf = self;
+    NSBlockOperation *op1 = [NSBlockOperation blockOperationWithBlock:^{
+        [weakSelf saleTicketNotSafe];
+    }];
+
+    // 4.创建卖票操作 op2
+    NSBlockOperation *op2 = [NSBlockOperation blockOperationWithBlock:^{
+        [weakSelf saleTicketNotSafe];
+    }];
+
+    // 5.添加操作，开始卖票
+    [queue1 addOperation:op1];
+    [queue2 addOperation:op2];
+}
+
+/**
+ * 售卖火车票(非线程安全)
+ */
+- (void)saleTicketNotSafe {
+    while (1) {
+
+        if (self.ticketSurplusCount > 0) {
+            //如果还有票，继续售卖
+            self.ticketSurplusCount--;
+            NSLog(@"%@", [NSString stringWithFormat:@"剩余票数:%ld 窗口:%@", self.ticketSurplusCount, [NSThread currentThread]]);
+            [NSThread sleepForTimeInterval:0.2];
+        } else {
+            NSLog(@"所有火车票均已售完");
+            break;
+        }
+    }
+}
+
+
+
+//   出现了，典型的双花
+
+
+
+//  一张票，卖给多个人
+
+
+
+//
+
+//
+
+
+//
+
+
+
+/**
+ * 线程安全：不使用 NSLock
+ * 初始化火车票数量、卖票窗口(非线程安全)、并开始卖票
+ */
+- (void)initTicketStatusSafe {
+    NSLog(@"currentThread---%@",[NSThread currentThread]); // 打印当前线程
+    self.simpleLock = [[NSLock alloc] init];
+    self.ticketSurplusCount = 50;
+
+    // 1.创建 queue1,queue1 代表北京火车票售卖窗口
+    NSOperationQueue *queue1 = [[NSOperationQueue alloc] init];
+    queue1.maxConcurrentOperationCount = 1;
+
+    // 2.创建 queue2,queue2 代表上海火车票售卖窗口
+    NSOperationQueue *queue2 = [[NSOperationQueue alloc] init];
+    queue2.maxConcurrentOperationCount = 1;
+
+    // 3.创建卖票操作 op1
+    __weak typeof(self) weakSelf = self;
+    NSBlockOperation *op1 = [NSBlockOperation blockOperationWithBlock:^{
+        [weakSelf saleTicketSafe];
+    }];
+
+    // 4.创建卖票操作 op2
+    NSBlockOperation *op2 = [NSBlockOperation blockOperationWithBlock:^{
+        [weakSelf saleTicketSafe];
+    }];
+
+    // 5.添加操作，开始卖票
+    [queue1 addOperation:op1];
+    [queue2 addOperation:op2];
+}
+
+/**
+ * 售卖火车票(非线程安全)
+ */
+- (void)saleTicketSafe {
+    while (1) {
+
+        [self.simpleLock lock];
+        if (self.ticketSurplusCount > 0) {
+            //如果还有票，继续售卖
+            self.ticketSurplusCount--;
+            NSLog(@"%@", [NSString stringWithFormat:@"剩余票数:%ld 窗口:%@", self.ticketSurplusCount, [NSThread currentThread]]);
+            [NSThread sleepForTimeInterval:0.2];
+        }
+        
+        [self.simpleLock unlock];
+        
+        if (self.ticketSurplusCount <= 0) {
+            NSLog(@"所有火车票均已售完");
+            break;
+        }
+    }
+    
+}
+
+
+
+
+//
+
+//
+
+
+//
 
 
 
@@ -62,8 +210,12 @@
 
 
     NSLog(@"开始 \n\n\nha");
-    [self addDependency];
-    NSLog(@"ha \n\n\n结束");
+    // [self addDependency];
+    // NSLog(@"ha \n\n\n结束");
+    
+    
+  //  [self initTicketStatusNotSave];
+    [self initTicketStatusSafe];
 
 }
 
