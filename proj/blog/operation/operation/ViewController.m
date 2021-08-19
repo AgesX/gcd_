d912d72e7da0a97b684d863ce7e42098994a78dc
邓江洲 <dengjiangzhouReal@gmail.com>
2021-08-19 17:18:32 +0800

    信号量  NSLock

--- proj/blog/operation/operation/ViewController.m
+++ proj/blog/operation/operation/ViewController.m
@@ -12,6 +12,7 @@
 @property (nonatomic, assign) NSInteger ticketSurplusCount;
 
 @property (nonatomic, strong) NSLock * simpleLock;
+@property (nonatomic, strong) dispatch_semaphore_t signal;
 
 @end
 
@@ -137,7 +138,7 @@
 
 
 /**
- * 线程安全：不使用 NSLock
+ * 线程安全：使用 NSLock
  * 初始化火车票数量、卖票窗口(非线程安全)、并开始卖票
  */
 - (void)initTicketStatusSafe {
@@ -170,7 +171,7 @@
 }
 
 /**
- * 售卖火车票(非线程安全)
+ * 售卖火车票(线程安全)
  */
 - (void)saleTicketSafe {
     while (1) {
@@ -204,6 +205,65 @@
 //
 
 
+/**
+ * 线程安全：不使用 NSLock
+ * 初始化火车票数量、卖票窗口(非线程安全)、并开始卖票
+ */
+- (void)initTicketStatusSafeSema {
+    self.signal = dispatch_semaphore_create(1);
+    NSLog(@"currentThread---%@",[NSThread currentThread]); // 打印当前线程
+
+    self.ticketSurplusCount = 50;
+
+    // 1.创建 queue1,queue1 代表北京火车票售卖窗口
+    NSOperationQueue *queue1 = [[NSOperationQueue alloc] init];
+    queue1.maxConcurrentOperationCount = 1;
+
+    // 2.创建 queue2,queue2 代表上海火车票售卖窗口
+    NSOperationQueue *queue2 = [[NSOperationQueue alloc] init];
+    queue2.maxConcurrentOperationCount = 1;
+
+    // 3.创建卖票操作 op1
+    __weak typeof(self) weakSelf = self;
+    NSBlockOperation *op1 = [NSBlockOperation blockOperationWithBlock:^{
+        [weakSelf saleTicketSafe_sema];
+    }];
+
+    // 4.创建卖票操作 op2
+    NSBlockOperation *op2 = [NSBlockOperation blockOperationWithBlock:^{
+        [weakSelf saleTicketSafe_sema];
+    }];
+
+    // 5.添加操作，开始卖票
+    [queue1 addOperation:op1];
+    [queue2 addOperation:op2];
+}
+
+/**
+ * 售卖火车票(线程安全)
+ */
+- (void)saleTicketSafe_sema{
+    while (1) {
+
+        dispatch_semaphore_wait(self.signal, DISPATCH_TIME_FOREVER); // 1
+        if (self.ticketSurplusCount > 0) {
+            //如果还有票，继续售卖
+            self.ticketSurplusCount--;
+            NSLog(@"%@", [NSString stringWithFormat:@"剩余票数:%ld 窗口:%@", self.ticketSurplusCount, [NSThread currentThread]]);
+            [NSThread sleepForTimeInterval:0.2];
+           //  dispatch_semaphore_signal(self.signal);  //  我写的
+        } else {
+            NSLog(@"所有火车票均已售完");
+            dispatch_semaphore_signal(self.signal);
+            break;
+        }
+      //   dispatch_semaphore_wait(self.signal, DISPATCH_TIME_FOREVER); //  我写的
+        
+        dispatch_semaphore_signal(self.signal);  // 2
+    }
+    
+}
+
 
 - (void)viewDidLoad {
     [super viewDidLoad];
@@ -215,7 +275,8 @@
     
     
   //  [self initTicketStatusNotSave];
-    [self initTicketStatusSafe];
+//    [self initTicketStatusSafe];
+    [self initTicketStatusSafeSema];
 
 }
 
